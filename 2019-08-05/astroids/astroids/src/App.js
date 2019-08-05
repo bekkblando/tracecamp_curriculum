f7d40747658f3f2eb584bec20a31d0748582025e
bekkblando <bekkblando@gmail.com>
2019-08-05 12:30:59 -0400

    input linked to state

--- 2019-08-05/astroids/astroids/src/App.js
+++ 2019-08-05/astroids/astroids/src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { getAstroids } from './nasaWorker';
+import moment from 'moment';
 
 function App() {
 
-  getAstroids("2019-08-01", "2019-08-05").then(
-    (data) => console.log(data),
-    (error) => console.error(error)
-  )
+  const [ startDate, setStartDate ] = useState(moment().format("YYYY-MM-DD"));
+
+
+  useEffect(() => {
+    console.log("This is coming from the useEffect:", startDate);
+  })
+
+
+  // getAstroids("2019-08-01", "2019-08-05").then(
+  //   (data) => console.log(data),
+  //   (error) => console.error(error)
+  // )
 
   return (
     <div className="App">
       Test
+      <input value = {startDate.toString()} type="date" id="dt" onChange={ (e) => setStartDate(e.target.value)  }/>
     </div>
   );
 }
