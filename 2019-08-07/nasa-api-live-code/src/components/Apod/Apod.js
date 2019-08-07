37f51811edc86e44ace02ea16612a68ac62a1aab
John Armstrong <jlarmst@g.clemson.edu>
2019-08-07 11:34:21 -0400

    feat: live code

--- 2019-08-07/nasa-api-live-code/src/components/Apod/Apod.js
+++ 2019-08-07/nasa-api-live-code/src/components/Apod/Apod.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 
 const API_KEY = 'GDo3b5eaKrQv9vrlXeeOlRDPjNK1k8a4Vg6f0aoU';
 
 const Apod = ({ match }) => {
   const date = match.params.date;
+  const [data, setData] = React.useState({});
+
+  React.useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?date=${date}&api_key=${API_KEY}`,
+      )
+      .then(response => {
+        setData(response.data);
+      });
+  }, [date]);
+
+  console.log(data);
+
   return <div>{date}</div>;
 };
 
