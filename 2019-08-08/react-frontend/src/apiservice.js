08442aa60616f17ef0b1047e3435988f3a9e15e3
damaliboyd <damaliboyd@ymail.com>
2019-08-08 11:12:02 -0400

    detail

--- 2019-08-08/react-frontend/src/apiservice.js
+++ 2019-08-08/react-frontend/src/apiservice.js
@@ -4,7 +4,11 @@ const API_URL = "http://127.0.0.1:8000/kickstarterdata";
 const config = {};
 
 function getKick(id) {
-  return axios.get(`${API_URL}/${id}`, config);
+  return axios.get(`${API_URL}/${id}/`, config);
 }
 
-export { getKick };
+function listKick() {
+  return axios.get(`${API_URL}/`, config);
+}
+
+export { getKick, listKick };
--- 2019-08-08/react-frontend/src/components/Detail.js
+++ 2019-08-08/react-frontend/src/components/Detail.js
@@ -9,12 +9,16 @@ function Detail(props) {
     const id = props.match.params.id;
     getKick(id).then(res => {
       setKick(res.data);
+      console.log(res.data);
     });
   }, []);
 
   return (
     <div className="container">
       <h1 className="text-center">Detail</h1>
+      <h3>Blurb: {kick.blurb}</h3>
+      <h3>Backers: {kick.backers}</h3>
+      <h3>Pledged: {kick.pledged}</h3>
     </div>
   );
 }
